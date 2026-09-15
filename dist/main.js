1ce38353edc6ca054fe4ba759db8f0ddee6028cf
CComps <tt.tomicraft@gmail.com>
2026-09-15 17:34:24 +0200

    added nice animations

--- dist/main.js
+++ dist/main.js
@@ -12,6 +12,22 @@ for (let row = 0; row < 8; row++) {
     } else {
       square.classList.add("black");
     }
+    for (let col = 0; col < 8; col++) {
+      let square = document.createElement("div");
+      // square.class("square");
+      // square.className("square");
+      // square.classList("square");
+      square.classList.add("square");
+      // if ((row % 2 == 0 && col % 2 == 0) || (row % 2 == 1 && col % 2 == 1)) {
+      if ((row + col) % 2 == 0) {
+        square.classList.add("white");
+      } else {
+        square.classList.add("black");
+      }
+      let index = row + col;
+      square.style.animationDelay = `${index * 0.05}s`;
+      board.appendChild(square);
+    }
     board.appendChild(square);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,22 @@ let board = document.getElementById("chessboard") as HTMLElement;
 for (let row = 0; row < 8; row++) {
   for (let col = 0; col < 8; col++) {
     let square = document.createElement("div");
+
     // square.class("square");
     // square.className("square");
-    // square.classList.add("square");
+    // square.classList("square");
+    square.classList.add("square");
+
     // if ((row % 2 == 0 && col % 2 == 0) || (row % 2 == 1 && col % 2 == 1)) {
     if ((row + col) % 2 == 0) {
       square.classList.add("white");
     } else {
       square.classList.add("black");
     }
+
+    let index = row + col;
+    square.style.animationDelay = `${index * 0.05}s`;
+
     board.appendChild(square);
   }
 }
