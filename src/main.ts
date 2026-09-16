85c827d4cba84d91062d390cecf8f48471041ded
CComps <tt.tomicraft@gmail.com>
2026-09-16 20:17:56 +0200

    chess peaces but still in letters

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 let board = document.getElementById("chessboard") as HTMLElement;
 
+// let piacees_in_ascii =
+
 for (let row = 0; row < 8; row++) {
   for (let col = 0; col < 8; col++) {
     let square = document.createElement("div");
@@ -18,6 +20,17 @@ for (let row = 0; row < 8; row++) {
 
     let index = row + col;
     square.style.animationDelay = `${index * 0.05}s`;
+
+    let chess_peace = document.createElement("p");
+    const pieces_black = ["R", "N", "B", "Q", "&#9812;", "B", "N", "R", "P"];
+    const pieces_white = ["R", "N", "B", "Q", "&#9812;", "B", "N", "R", "P"];
+    if (row === 0) chess_peace.textContent = pieces_black[col];
+    else if (row === 1) chess_peace.textContent = pieces_black[8];
+    else if (row === 6) chess_peace.textContent = pieces_white[8];
+    else if (row === 7) chess_peace.textContent = pieces_white[col];
+    chess_peace.classList.add("piece");
+
+    square.appendChild(chess_peace);
 
     board.appendChild(square);
   }
