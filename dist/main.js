c7560176a0a61b3c84903d1646b59fe462f34bbf
CComps <tt.tomicraft@gmail.com>
2026-09-18 07:48:43 +0200

    added numebrs and letters

--- dist/main.js
+++ dist/main.js
@@ -1,52 +1,80 @@
 "use strict";
 let board = document.getElementById("chessboard");
-// let piacees_in_ascii =
-for (let row = 0; row < 8; row++) {
-    for (let col = 0; col < 8; col++) {
-        let square = document.createElement("div");
-        // square.class("square");
-        // square.className("square");
-        // square.classList("square");
-        square.classList.add("square");
-        // if ((row % 2 == 0 && col % 2 == 0) || (row % 2 == 1 && col % 2 == 1)) {
-        if ((row + col) % 2 == 0) {
-            square.classList.add("white");
+let row_numbers = document.getElementById("row-numbers");
+for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+        let internal_row = row != 9 ? row : row;
+        let internal_col = col != 9 ? col - 1 : col;
+        if (row != 8 && col != 0) {
+            let chess_peace = document.createElement("p");
+            const pieces = [
+                "\u265C",
+                "\u265E",
+                "\u265D",
+                "\u265B",
+                "\u265A",
+                "\u265D",
+                "\u265E",
+                "\u265C",
+                "\u265F",
+            ];
+            let square = document.createElement("div");
+            // square.class("square");
+            // square.className("square");
+            // square.classList("square");
+            square.classList.add("square");
+            // if ((internal_row % 2 == 0 && internal_col % 2 == 0) || (row % 2 == 1 && col % 2 == 1)) {
+            if ((internal_row + internal_col) % 2 == 0) {
+                square.classList.add("white");
+            }
+            else {
+                square.classList.add("black");
+            }
+            let index = internal_row + internal_col;
+            square.style.animationDelay = `${index * 0.05}s`;
+            if (internal_row === 0) {
+                chess_peace.textContent = pieces[internal_col];
+                chess_peace.classList.add("black-piece");
+            }
+            else if (internal_row === 1) {
+                chess_peace.textContent = pieces[8];
+                chess_peace.classList.add("black-piece");
+            }
+            else if (internal_row === 6) {
+                chess_peace.textContent = pieces[8];
+                chess_peace.classList.add("white-piece");
+            }
+            else if (internal_row === 7) {
+                chess_peace.textContent = pieces[internal_col];
+                chess_peace.classList.add("white-piece");
+            }
+            chess_peace.classList.add("piece");
+            square.appendChild(chess_peace);
+            board.appendChild(square);
+            continue;
         }
-        else {
-            square.classList.add("black");
+        if (col == 0 && row != 8) {
+            let board_y_number = (() => {
+                let a = [];
+                for (let i = 1; i <= 8; i++)
+                    a.push(i);
+                return a;
+            })();
+            let internal_col_number = document.createElement("div");
+            internal_col_number.classList.add("center");
+            internal_col_number.classList.add("col");
+            internal_col_number.textContent =
+                board_y_number[internal_row]?.toString() || "";
+            board.appendChild(internal_col_number);
+            continue;
         }
-        let index = row + col;
-        square.style.animationDelay = `${index * 0.05}s`;
-        let chess_peace = document.createElement("p");
-        const pieces_black = [
-            "\u265C",
-            "\u265E",
-            "\u265D",
-            "\u265B",
-            "\u265A",
-            "\u265D",
-            "\u265E",
-            "\u265C",
-            "\u265F",
-        ];
-        if (row === 0) {
-            chess_peace.textContent = pieces_black[col];
-            chess_peace.classList.add("black-piece");
-        }
-        else if (row === 1) {
-            chess_peace.textContent = pieces_black[8];
-            chess_peace.classList.add("black-piece");
-        }
-        else if (row === 6) {
-            chess_peace.textContent = pieces_black[8];
-            chess_peace.classList.add("white-piece");
-        }
-        else if (row === 7) {
-            chess_peace.textContent = pieces_black[col];
-            chess_peace.classList.add("white-piece");
-        }
-        chess_peace.classList.add("piece");
-        square.appendChild(chess_peace);
-        board.appendChild(square);
+        let board_x_number = ["a", "b", "c", "d", "e", "f", "g", "h"];
+        let internal_row_number = document.createElement("div");
+        internal_row_number.classList.add("numbered");
+        internal_row_number.classList.add("row");
+        internal_row_number.textContent =
+            board_x_number[internal_col]?.toString() || "";
+        internal_row_number.style.animationDelay = `${internal_col * 0.05}s`;
+        board.appendChild(internal_row_number);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,88 @@
 let board = document.getElementById("chessboard") as HTMLElement;
+let row_numbers = document.getElementById("row-numbers") as HTMLElement;
 
-// let piacees_in_ascii =
+for (let row = 0; row < 9; row++) {
+  for (let col = 0; col < 9; col++) {
+    let internal_row = row != 9 ? row : row;
+    let internal_col = col != 9 ? col - 1 : col;
 
-for (let row = 0; row < 8; row++) {
-  for (let col = 0; col < 8; col++) {
-    let square = document.createElement("div");
+    if (row != 8 && col != 0) {
+      let chess_peace = document.createElement("p");
+      const pieces = [
+        "\u265C",
+        "\u265E",
+        "\u265D",
+        "\u265B",
+        "\u265A",
+        "\u265D",
+        "\u265E",
+        "\u265C",
+        "\u265F",
+      ];
 
-    // square.class("square");
-    // square.className("square");
-    // square.classList("square");
-    square.classList.add("square");
+      let square = document.createElement("div");
 
-    // if ((row % 2 == 0 && col % 2 == 0) || (row % 2 == 1 && col % 2 == 1)) {
-    if ((row + col) % 2 == 0) {
-      square.classList.add("white");
-    } else {
-      square.classList.add("black");
+      // square.class("square");
+      // square.className("square");
+      // square.classList("square");
+      square.classList.add("square");
+
+      // if ((internal_row % 2 == 0 && internal_col % 2 == 0) || (row % 2 == 1 && col % 2 == 1)) {
+      if ((internal_row + internal_col) % 2 == 0) {
+        square.classList.add("white");
+      } else {
+        square.classList.add("black");
+      }
+
+      let index = internal_row + internal_col;
+      square.style.animationDelay = `${index * 0.05}s`;
+
+      if (internal_row === 0) {
+        chess_peace.textContent = pieces[internal_col];
+        chess_peace.classList.add("black-piece");
+      } else if (internal_row === 1) {
+        chess_peace.textContent = pieces[8];
+        chess_peace.classList.add("black-piece");
+      } else if (internal_row === 6) {
+        chess_peace.textContent = pieces[8];
+        chess_peace.classList.add("white-piece");
+      } else if (internal_row === 7) {
+        chess_peace.textContent = pieces[internal_col];
+        chess_peace.classList.add("white-piece");
+      }
+      chess_peace.classList.add("piece");
+
+      square.appendChild(chess_peace);
+
+      board.appendChild(square);
+
+      continue;
     }
 
-    let index = row + col;
-    square.style.animationDelay = `${index * 0.05}s`;
+    if (col == 0 && row != 8) {
+      let board_y_number: number[] = (() => {
+        let a = [];
+        for (let i = 1; i <= 8; i++) a.push(i);
+        return a;
+      })();
 
-    let chess_peace = document.createElement("p");
-    const pieces_black = [
-      "\u265C",
-      "\u265E",
-      "\u265D",
-      "\u265B",
-      "\u265A",
-      "\u265D",
-      "\u265E",
-      "\u265C",
-      "\u265F",
-    ];
-    if (row === 0) {
-      chess_peace.textContent = pieces_black[col];
-      chess_peace.classList.add("black-piece");
-    } else if (row === 1) {
-      chess_peace.textContent = pieces_black[8];
-      chess_peace.classList.add("black-piece");
-    } else if (row === 6) {
-      chess_peace.textContent = pieces_black[8];
-      chess_peace.classList.add("white-piece");
-    } else if (row === 7) {
-      chess_peace.textContent = pieces_black[col];
-      chess_peace.classList.add("white-piece");
+      let internal_col_number = document.createElement("div");
+      internal_col_number.classList.add("center");
+      internal_col_number.classList.add("col");
+      internal_col_number.textContent =
+        board_y_number[internal_row]?.toString() || "";
+      board.appendChild(internal_col_number);
+      continue;
     }
-    chess_peace.classList.add("piece");
 
-    square.appendChild(chess_peace);
+    let board_x_number = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
-    board.appendChild(square);
+    let internal_row_number = document.createElement("div");
+    internal_row_number.classList.add("numbered");
+    internal_row_number.classList.add("row");
+    internal_row_number.textContent =
+      board_x_number[internal_col]?.toString() || "";
+    internal_row_number.style.animationDelay = `${internal_col * 0.05}s`;
+    board.appendChild(internal_row_number);
   }
 }
