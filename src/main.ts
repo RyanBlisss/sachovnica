b1685964ee68baef87bef84906d548ade06ae95a
CComps <tt.tomicraft@gmail.com>
2026-09-19 14:37:07 +0200

    animation update again. including selecting squares and numbers/letters animate sequencially

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ for (let row = 0; row < 9; row++) {
       }
 
       const index = internal_row + internal_col;
-      square.style.animationDelay = `${index * 0.05}s`;
+      square.style.animationDelay = `${index * 0.08}s`;
 
       if (internal_row === 0) {
         chess_peace.textContent = pieces[internal_col];
@@ -73,7 +73,9 @@ for (let row = 0; row < 9; row++) {
     if (col == 0 && row != 8) {
       const board_y_number: number[] = (() => {
         let a = [];
-        for (let i = 1; i <= 8; i++) a.push(i);
+        for (let i = 1; i <= 8; i++) {
+          a.push(i);
+        }
         return a;
       })();
 
@@ -82,7 +84,7 @@ for (let row = 0; row < 9; row++) {
       internal_col_number.classList.add("col");
       internal_col_number.textContent =
         board_y_number[internal_row]?.toString() || "";
-      internal_col_number.style.animationDelay = `${internal_row * 0.1}s`;
+      internal_col_number.style.animationDelay = `${internal_row * 0.1 + 1}s`;
       board.appendChild(internal_col_number);
       continue;
     }
@@ -94,7 +96,7 @@ for (let row = 0; row < 9; row++) {
     internal_row_number.classList.add("row");
     internal_row_number.textContent =
       board_x_number[internal_col]?.toString() || "";
-    internal_row_number.style.animationDelay = `${internal_col * 0.1}s`;
+    internal_row_number.style.animationDelay = `${internal_col * 0.1 + 1.8}s`;
     board.appendChild(internal_row_number);
   }
 }
